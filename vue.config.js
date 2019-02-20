ed97ab00bd9f16bae05a5ab5c593ed3663c098cb
xuliangzhan <xu_liangzhan@163.com>
2019-02-20 14:35:04 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-mock-template/' : '/',
   outputDir: 'docs',
+  configureWebpack: {
+    performance: {
+      hints: false
+    }
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
