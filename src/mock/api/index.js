e720ff0e2762e5c2e4ce05416edf0f34a180f5fb
xuliangzhan <xu_liangzhan@163.com>
2019-02-21 19:57:34 +0800

    update

--- src/mock/api/index.js
+++ src/mock/api/index.js
@@ -1,9 +1,22 @@
-import { GET } from 'xe-ajax-mock'
+import XEUtils from 'xe-utils'
+import { template, DELETE, GET } from 'xe-ajax-mock'
+
+function removeByPathVariable (list, key) {
+  return function (request, response, { pathVariable }) {
+    let index = XEUtils.findIndexOf(list, item => item[key] !== pathVariable[key])
+    response.body = XEUtils.remove(list, index)
+    return response
+  }
+}
 
 // role api
-GET('/api/role/list', require('./role/list.json'))
+let ruleList = template(require('./role/list.json'))
+DELETE('/api/role/delete/{id}', removeByPathVariable(ruleList, 'id'))
+GET('/api/role/list', ruleList)
 GET('/api/role/page/list/{pageSize}/{currentPage}', require('./role/page.json'))
 
 // user api
-GET('/api/user/list', require('./user/list.json'))
+let userList = template(require('./user/list.json'))
+DELETE('/api/user/delete/{id}', removeByPathVariable(userList, 'id'))
+GET('/api/user/list', userList)
 GET('/api/user/page/list/{pageSize}/{currentPage}', require('./user/page.json'))
--- src/views/role/RoleList.vue
+++ src/views/role/RoleList.vue
@@ -6,7 +6,7 @@
       :data="list"
       stripe
       border
-      height="400"
+      height="534"
       style="width: 100%">
       <el-table-column
         prop="id"
@@ -32,12 +32,26 @@
         label="说明"
         show-overflow-tooltip>
       </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="120">
+        <template slot-scope="scope">
+          <el-button
+            @click.native.prevent="deleteRowEvent(scope.row)"
+            type="text"
+            size="small">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
 import XEAjax from 'xe-ajax'
+import { MessageBox, Message } from 'element-ui'
 
 export default {
   data () {
@@ -57,6 +71,30 @@ export default {
         this.loading = false
       }).catch(e => {
         this.loading = false
+      })
+    },
+    deleteRowEvent (row) {
+      MessageBox.confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.loading = true
+        XEAjax.deleteJSON(`/api/role/delete/${row.id}`).then(data => {
+          Message({
+            type: 'success',
+            message: '删除成功!'
+          })
+          this.list = this.list.filter(item => item !== row)
+          this.loading = false
+        }).catch(e => {
+          this.loading = false
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '已取消删除'
+        })
       })
     }
   }
