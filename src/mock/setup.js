543b3ed42f64cc041f09c36fe253e5377c987953
xuliangzhan <xu_liangzhan@163.com>
2019-02-22 23:49:57 +0800

    update

--- src/mock/setup.js
+++ src/mock/setup.js
@@ -8,12 +8,3 @@ XEAjaxMock.setup({
   error: true,
   log: true
 })
-
-XEAjaxMock.template.mixin({
-  getPageId ({ currentPage, pageSize }, $index) {
-    return (currentPage * pageSize) + $index + 1
-  },
-  getPageList ({ currentPage, pageSize }, totalResult) {
-    return (currentPage * pageSize) > totalResult ? (totalResult + pageSize - currentPage * pageSize) : pageSize
-  }
-})
