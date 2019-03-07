057f7ce9bac1eddbf2c0aa2582f4cf9c21e620ed
xuliangzhan <xu_liangzhan@163.com>
2019-03-07 14:48:05 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-mock-template/' : '/',
   outputDir: 'docs',
+  assetsDir: 'static',
   productionSourceMap: false,
   configureWebpack: {
     performance: {
