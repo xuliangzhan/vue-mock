d8d2d697f45b4eb7e6ea9aa2e6bd49b0200414fd
xuliangzhan <xu_liangzhan@163.com>
2019-03-15 16:21:01 +0800

    update

--- src/mock/api/index.js
+++ src/mock/api/index.js
@@ -1,117 +1,45 @@
-import XEUtils from 'xe-utils'
 import { template, DELETE, POST, GET } from 'xe-ajax-mock'
+import MockUtil from './util'
 
-var idIndex = 10000
-
-function sortList (list) {
-  return XEUtils.sortBy(list, 'updateTime').reverse()
-}
-
-// 删除单条
-function removeByPathVariable (list, itemKey) {
-  return function (request, response, { pathVariable }) {
-    let rest = []
-    if (pathVariable) {
-      rest = XEUtils.remove(list, item => item[itemKey] === pathVariable[itemKey])
-    }
-    response.body = rest
-    return response
+class RoleVO {
+  constructor (data) {
+    this.id = data.id
+    this.name = data.name
+    this.describe = data.describe
+    this.createTime = data.createTime
+    this.updateTime = data.updateTime
   }
 }
+const roleList = template(require('./role/list.json'))
+DELETE('/api/role/delete/{id}', MockUtil.deleteByPathVariable(roleList, RoleVO, 'id'))
+POST('/api/role/add', MockUtil.insertByBody(roleList, RoleVO, 'id'))
+POST('/api/role/update', MockUtil.updateByBody(roleList, RoleVO, 'id'))
+POST('/api/role/save', MockUtil.saveListByBody(roleList, RoleVO, 'id'))
+GET('/api/role/list', MockUtil.findDescSortList(roleList, RoleVO, ['updateTime'], 10))
+GET('/api/role/page/list/{pageSize}/{currentPage}', MockUtil.findDescSortPageList(roleList, RoleVO, ['updateTime']))
 
-// 批量删除
-function removeListByBody (list, itemKey) {
-  return function (request, response) {
-    let rest = []
-    if (request.body) {
-      let removeRecords = request.body.removeRecords
-      rest = XEUtils.remove(list, item => removeRecords.some(row => row[itemKey] === item[itemKey]))
-    }
-    response.body = rest
-    return response
+class UserVO {
+  constructor (data) {
+    this.id = data.id
+    this.name = data.name
+    this.password = data.password
+    this.sex = data.sex
+    this.role = data.role
+    this.region = data.region
+    this.email = data.email
+    this.age = data.age
+    this.flag = data.flag
+    this.describe = data.describe
+    this.describe2 = data.describe2
+    this.describe3 = data.describe3
+    this.createTime = data.createTime
+    this.updateTime = data.updateTime
   }
 }
-
-// 批量更新
-function updateListByBody (list, itemKey) {
-  return function (request, response) {
-    let rest = []
-    if (request.body) {
-      let updateRecords = request.body.updateRecords
-      updateRecords.forEach(data => {
-        let item = list.find(item => item[itemKey] === data[itemKey])
-        if (item) {
-          XEUtils.destructuring(item, data, { updateTime: Date.now() })
-          rest.push(item)
-        }
-      })
-    }
-    response.body = rest
-    return response
-  }
-}
-
-// 批量保存
-function saveListByBody (list, itemKey) {
-  return function (request, response) {
-    let updateRest = []
-    let removeRest = []
-    if (request.body) {
-      let updateTime = Date.now()
-      let updateRecords = request.body.updateRecords
-      let removeRecords = request.body.removeRecords
-      let insertRecords = request.body.insertRecords
-      removeRest = XEUtils.remove(list, item => removeRecords.some(row => row[itemKey] === item[itemKey]))
-      updateRecords.forEach(data => {
-        let item = list.find(item => item[itemKey] === data[itemKey])
-        if (item) {
-          XEUtils.destructuring(item, data, { updateTime })
-          updateRest.push(item)
-        }
-      })
-      insertRecords.forEach(data => {
-        list.push(Object.assign({}, data, { [itemKey]: idIndex++, updateTime, createDate: updateTime }))
-      })
-    }
-    response.body = { updateRest, removeRest }
-    return response
-  }
-}
-
-// 处理分页
-function getPageList (list, propSize, propCurrent) {
-  return function (request, response, { pathVariable }) {
-    let pageSize = 10
-    let currentPage = 1
-    let totalResult = list.length
-    let rest = []
-    if (pathVariable) {
-      pageSize = XEUtils.toNumber(pathVariable[propSize])
-      currentPage = XEUtils.toNumber(pathVariable[propCurrent])
-      rest = sortList(list).slice((currentPage - 1) * pageSize, currentPage * pageSize)
-    }
-    response.body = {
-      page: { pageSize, currentPage, totalResult },
-      result: rest
-    }
-    return response
-  }
-}
-
-// role api
-let ruleList = template(require('./role/list.json'))
-DELETE('/api/role/delete/{id}', removeByPathVariable(ruleList, 'id'))
-POST('/api/role/delete', removeListByBody(ruleList, 'id'))
-POST('/api/role/update', updateListByBody(ruleList, 'id'))
-POST('/api/role/save', saveListByBody(ruleList, 'id'))
-GET('/api/role/list', request => sortList(ruleList).slice(0, 10))
-GET('/api/role/page/list/{pageSize}/{currentPage}', getPageList(ruleList, 'pageSize', 'currentPage'))
-
-// user api
-let userList = template(require('./user/list.json'))
-DELETE('/api/user/delete/{id}', removeByPathVariable(userList, 'id'))
-POST('/api/user/delete', removeListByBody(userList, 'id'))
-POST('/api/user/update', updateListByBody(userList, 'id'))
-POST('/api/user/save', saveListByBody(userList, 'id'))
-GET('/api/user/list', request => sortList(userList).slice(0, 10))
-GET('/api/user/page/list/{pageSize}/{currentPage}', getPageList(userList, 'pageSize', 'currentPage'))
+const userList = template(require('./user/list.json'))
+DELETE('/api/user/delete/{id}', MockUtil.deleteByPathVariable(userList, UserVO, 'id'))
+POST('/api/user/add', MockUtil.insertByBody(userList, UserVO, 'id'))
+POST('/api/user/update', MockUtil.updateByBody(userList, UserVO, 'id'))
+POST('/api/user/save', MockUtil.saveListByBody(userList, UserVO, 'id'))
+GET('/api/user/list', MockUtil.findDescSortList(userList, UserVO, ['updateTime'], 10))
+GET('/api/user/page/list/{pageSize}/{currentPage}', MockUtil.findDescSortPageList(userList, UserVO, ['updateTime']))
---
+++ src/mock/api/util.js
@@ -0,0 +1,156 @@
+import XEUtils from 'xe-utils'
+
+var idIndex = 100000
+
+const MockUtil = {
+  // 获取最新数据、升序
+  findAscSortList (list, ModelVO, sortProps, max) {
+    return function (request, response) {
+      let rest = list
+      let params = request.params
+      let filterProps = XEUtils.keys(params).filter(key => params[key])
+      if (filterProps) {
+        rest = rest.filter(data => filterProps.every(key => '' + data[key] === '' + params[key]))
+      }
+      rest = XEUtils.sortBy(list, sortProps)
+      return max ? rest.slice(0, max) : rest
+    }
+  },
+  // 获取最新数据、倒序
+  findDescSortList (list, ModelVO, sortProps, max) {
+    return function (request, response) {
+      let rest = list
+      let params = request.params
+      let filterProps = XEUtils.keys(params).filter(key => params[key])
+      if (filterProps) {
+        rest = rest.filter(data => filterProps.every(key => '' + data[key] === '' + params[key]))
+      }
+      rest = XEUtils.sortBy(list, sortProps).reverse()
+      return max ? rest.slice(0, max) : rest
+    }
+  },
+  // 删除单条
+  deleteByPathVariable (list, ModelVO, itemKey) {
+    return function (request, response, { pathVariable }) {
+      let rest = []
+      if (pathVariable) {
+        rest = XEUtils.remove(list, item => item[itemKey] === pathVariable[itemKey])
+      }
+      response.body = rest
+      return response
+    }
+  },
+  // 插入单条
+  insertByBody (list, ModelVO, itemKey) {
+    return function (request, response) {
+      let result = []
+      if (request.body) {
+        let updateTime = Date.now()
+        let insertRecords = [request.body]
+        insertRecords.forEach(data => {
+          let rest = Object.assign(new ModelVO(data), { [itemKey]: idIndex++, updateTime, createTime: updateTime })
+          result.push(rest)
+          list.push(rest)
+        })
+      }
+      return result
+    }
+  },
+  // 更新单条
+  updateByBody (list, ModelVO, itemKey) {
+    return function (request, response) {
+      let result = []
+      if (request.body) {
+        let updateTime = Date.now()
+        let updateRecords = [request.body]
+        updateRecords.forEach(data => {
+          let item = list.find(item => item[itemKey] === data[itemKey])
+          if (item) {
+            XEUtils.destructuring(item, data, { updateTime })
+            result.push(item)
+          }
+        })
+      }
+      return result
+    }
+  },
+  // 批量保存
+  saveListByBody (list, ModelVO, itemKey) {
+    return function (request, response) {
+      let insertRest = []
+      let updateRest = []
+      let removeRest = []
+      if (request.body) {
+        let updateTime = Date.now()
+        let updateRecords = request.body.updateRecords || []
+        let removeRecords = request.body.removeRecords || []
+        let insertRecords = request.body.insertRecords || []
+        removeRest = XEUtils.remove(list, item => removeRecords.some(row => row[itemKey] === item[itemKey]))
+        updateRecords.forEach(data => {
+          let item = list.find(item => item[itemKey] === data[itemKey])
+          if (item) {
+            XEUtils.destructuring(item, data, { updateTime })
+            updateRest.push(item)
+          }
+        })
+        insertRecords.forEach(data => {
+          let rest = Object.assign(new ModelVO(data), { [itemKey]: idIndex++, updateTime, createTime: updateTime })
+          insertRest.push(rest)
+          list.push(rest)
+        })
+      }
+      response.body = { insertRest, updateRest, removeRest }
+      return response
+    }
+  },
+  // 分页、升序
+  findAscSortPageList (list, ModelVO, sortProps, pageProp) {
+    return function (request, response, { pathVariable }) {
+      let pageSize = 10
+      let currentPage = 1
+      let rest = list
+      let params = request.params
+      let filterProps = XEUtils.keys(params).filter(key => params[key])
+      if (filterProps) {
+        rest = rest.filter(data => filterProps.every(key => '' + data[key] === '' + params[key]))
+      }
+      if (pathVariable) {
+        pageSize = XEUtils.toNumber(pathVariable[pageProp ? pageProp.size : 'pageSize']) || pageSize
+        currentPage = XEUtils.toNumber(pathVariable[pageProp ? pageProp.current : 'currentPage']) || currentPage
+      }
+      let totalResult = rest.length
+      rest = XEUtils.sortBy(list, sortProps).slice((currentPage - 1) * pageSize, currentPage * pageSize)
+      response.body = {
+        page: { pageSize, currentPage, totalResult },
+        result: rest
+      }
+      return response
+    }
+  },
+  // 分页、倒序
+  findDescSortPageList (list, ModelVO, sortProps, pageProp) {
+    return function (request, response, { pathVariable }) {
+      let pageSize = 10
+      let currentPage = 1
+      let rest = list
+      let params = request.params
+      let filterProps = XEUtils.keys(params).filter(key => params[key])
+      if (filterProps) {
+        rest = rest.filter(data => filterProps.every(key => String(data[key] || '').indexOf(params[key]) > -1))
+      }
+      if (pathVariable) {
+        pageSize = XEUtils.toNumber(pathVariable[pageProp ? pageProp.size : 'pageSize']) || pageSize
+        currentPage = XEUtils.toNumber(pathVariable[pageProp ? pageProp.current : 'currentPage']) || currentPage
+      }
+      let totalResult = rest.length
+      rest = XEUtils.sortBy(rest, sortProps).reverse().slice((currentPage - 1) * pageSize, currentPage * pageSize)
+      response.body = {
+        page: { pageSize, currentPage, totalResult },
+        result: rest
+      }
+      return response
+    }
+  }
+}
+
+export default MockUtil
