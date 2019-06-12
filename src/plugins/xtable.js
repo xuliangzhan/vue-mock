bd7767cd2a53581b6beb0d4a2a6d239e8a3a734b
xuliangzhan <xu_liangzhan@163.com>
2019-06-12 19:26:40 +0800

    update

--- src/plugins/xtable.js
+++ src/plugins/xtable.js
@@ -9,20 +9,3 @@ import 'vxe-table-plugin-iview/dist/style.css'
 Vue.use(VXETable)
 VXETable.use(VXETablePluginElement)
 VXETable.use(VXETablePluginIView)
-
-// 设置默认参数
-VXETable.setup({
-  scrollX: {
-    gt: 60,
-    oSize: 6,
-    rSize: 16,
-    vSize: 0
-  },
-  scrollY: {
-    gt: 500,
-    oSize: 25,
-    rSize: 70,
-    vSize: 0,
-    rHeight: 0
-  }
-})
