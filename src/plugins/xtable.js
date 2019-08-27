63b4d24d2da80b098f635edd0ef4eb90c67b3d55
xuliangzhan <xu_liangzhan@163.com>
2019-08-27 22:55:49 +0800

    update

--- src/plugins/xtable.js
+++ src/plugins/xtable.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 // import VXETable from 'vxe-table'
 import VXETablePluginElement from 'vxe-table-plugin-element'
+import VXETablePluginCharts from 'vxe-table-plugin-charts'
 import 'vxe-table-plugin-element/dist/style.css'
+import 'vxe-table-plugin-charts/dist/style.css'
 // import 'vxe-table/lib/index.css'
 
 import {
@@ -11,11 +13,14 @@ import {
   Header,
   Body,
   Icon,
+  Menu,
   Loading,
   Toolbar,
   Export,
   Pager,
   Button,
+  Modal,
+  Keyboard,
   Checkbox
 } from 'vxe-table'
 import zhCNLocat from 'vxe-table/lib/locale/lang/zh-CN'
@@ -29,12 +34,16 @@ Vue.use(Column)
 Vue.use(Header)
 Vue.use(Body)
 Vue.use(Icon)
+Vue.use(Menu)
 Vue.use(Loading)
 Vue.use(Toolbar)
 Vue.use(Export)
 Vue.use(Pager)
 Vue.use(Button)
 Vue.use(Checkbox)
+Vue.use(Modal)
+Vue.use(Keyboard)
 
 // Vue.use(VXETable)
 VXETable.use(VXETablePluginElement)
+VXETable.use(VXETablePluginCharts)
